8cb1e123314e7ef91b4f60cb4ed985713bedf992
sebgreenway <sebgreenway@gmail.com>
2025-06-09 18:28:50 +0100

    finished version with sys

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import word_count, character_count, sort_characters, display_results
 
 def get_book_text(path_to_file):
@@ -7,18 +8,19 @@ def get_book_text(path_to_file):
 
 def main():
     try:
-        book_text = get_book_text(book)
+        book_text = get_book_text(sys.argv[1])
         word_count_result = word_count(book_text)
         character_count_result = character_count(book_text)
         sorted_characters = sort_characters(character_count_result)
-        display_results(book, word_count_result, sorted_characters)
-        #print(f'{word_count_result} words found in the document')
-        #print(character_count_result)
+        display_results(sys.argv[1], word_count_result, sorted_characters)
     except FileNotFoundError as e:
         print(f'File not found: {e}')
     except Exception as e:
         print(f'An error has occured: {e}')
 
-book = 'books/frankenstein.txt'
+if len(sys.argv) < 2:
+    print('Usage: python3 main.py <path_to_book>')
+    sys.exit(1)
+
 
 main()
